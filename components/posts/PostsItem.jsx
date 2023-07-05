7f83956070a7a1058e4e244a0408d46a4e8144fa
jeff0518 <cycivs07@icloud.com>
2023-07-05 20:59:53 +0800

    fix: image

--- components/posts/PostsItem.jsx
+++ components/posts/PostsItem.jsx
@@ -18,7 +18,13 @@ function PostsItem(props) {
     <li className={classes.post}>
       <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200} />
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
