76b63f37461821fdb35e8b0a389bf6bd12c93611
jeff0518 <cycivs07@icloud.com>
2023-08-03 20:31:59 +0800

    fix: test build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "cycivs07",
+        mongodb_password: "aSLUEso4X9sjdLmJ",
+        mongodb_clustername: "cluster0",
+        mongodb_database: "my-site-dev",
+      },
+    };
+  }
+  return {
+    env: {
+      mongodb_username: "cycivs07",
+      mongodb_password: "aSLUEso4X9sjdLmJ",
+      mongodb_clustername: "cluster0",
+      mongodb_database: "my-site",
+    },
+  };
+};
